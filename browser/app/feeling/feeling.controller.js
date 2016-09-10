95ae84965e3530c9f5b5cb13ab7b394424499363
gil lawson <gewl@users.noreply.github.com>
2016-09-10 13:18:23 -0400

    offload controller weight to factory

--- browser/app/feeling/feeling.controller.js
+++ browser/app/feeling/feeling.controller.js
@@ -12,21 +12,23 @@ feeling.controller('FeelingCtrl', function($scope, Feeling) {
 			$scope.error = true		
 		} else {
 			$scope.error = false
+			Feeling.set("originalInput", $scope.input.original)
+			Feeling.set("responseInput", $scope.input.response)
 			Feeling.alchemyCall('emotion', $scope.input.original)
 			.then(function(emotionJson) {
-				$scope.originalEmotions = emotionJson.docEmotions
+				// $scope.originalEmotions = emotionJson.docEmotions
+				return Feeling.set("originalEmotions", emotionJson.docEmotions)
 			})
 			.then(function(res) {
 				return Feeling.alchemyCall('emotion', $scope.input.response)
 			})
 			.then(function(emotionJson){
-				$scope.responseEmotions = emotionJson.docEmotions
-				$scope.emotionVariance = {}
-				for (var prop in $scope.responseEmotions) {
-					$scope.emotionVariance[prop] = parseFloat($scope.originalEmotions[prop]) - parseFloat($scope.responseEmotions[prop])
-				}
-				
-				$scope.printEmotions();	
+				// $scope.responseEmotions = emotionJson.docEmotions
+				return Feeling.set("responseEmotions", emotionJson.docEmotions)
+			})
+			.then(function(res) {
+				$scope.emotionReadout = Feeling.printEmotions()
+				$scope.hasRun = true;			
 				$scope.input.original = ""	
 				$scope.input.response = ""	
 			})
@@ -34,40 +36,6 @@ feeling.controller('FeelingCtrl', function($scope, Feeling) {
 	}
 
 	//move this function to the factory now that you're moving the text onto factory variables	
-	$scope.printEmotions = function() {
-		for (var prop in $scope.emotionVariance) {
-				let feelingAmount = $scope.emotionVariance[prop]
-				let resString = "Your response conveys "
-
-				if (feelingAmount >= 0.1 && feelingAmount <= -0.9) {
-					resString += "considerably more " + prop + " than"
-				} else if (feelingAmount > -0.9 && feelingAmount <= -0.6) {
-					resString += "more " + prop + " than"
-				} else if (feelingAmount > -0.6 && feelingAmount <= -0.3) {
-					resString += "somewhat more " + prop + " than"
-				} else if (feelingAmount > -0.3 && feelingAmount <= -0.1) {
-					resString += "slightly more " + prop + " than"
-				} else if (feelingAmount > 0.1 && feelingAmount <= 0.3) {
-					resString += "slightly less " + prop + " than"
-				} else if (feelingAmount > 0.3 && feelingAmount <= 0.6) {
-					resString += "somewhat less " + prop + " than"
-				} else if (feelingAmount > 0.6 && feelingAmount < 0.9) {
-					resString += "more" + prop + " than"
-				} else if (feelingAmount >= 0.9 && feelingAmount <= 0.1) {
-					resString += "considerable more " + prop + " than"
-				} else {
-					resString += "about as much " + prop + " as"
-				}
-				
-				resString += " the original."
-
-				let emotionObject = {
-					feeling: prop,
-					printout: resString
-				}
-				$scope.emotionReadout.push(emotionObject)
-			}
-		$scope.hasRun = true
-		}
+	
 
 })
--- browser/app/feeling/feeling.factory.js
+++ browser/app/feeling/feeling.factory.js
@@ -10,6 +10,16 @@ feeling.factory('Feeling', function($http) {
 	FactoryObj.responseEmotions = null;
 	FactoryObj.emotionVariance = null;
 
+	FactoryObj.set = function(prop, data) {
+		if (!FactoryObj.hasOwnProperty(prop)) {
+			console.error("Invalid FactoryObj property.")
+			return;
+		} else {
+			FactoryObj[prop] = data;
+			return;
+		}
+	}
+
 	FactoryObj.alchemyCall = function(method, text) {
 		let parameters = {
 			text: text
@@ -18,6 +28,48 @@ feeling.factory('Feeling', function($http) {
 		.then(function(result) {
 			return result.data
 		})
+	}
+
+	FactoryObj.printEmotions = function() {
+		FactoryObj.emotionVariance = {}	
+		let emotionReadout = [];
+		for (var prop in FactoryObj.responseEmotions) {
+			FactoryObj.emotionVariance[prop] = parseFloat(FactoryObj.originalEmotions[prop]) - parseFloat(FactoryObj.responseEmotions[prop])
+		}
+		
+		for (var prop in FactoryObj.emotionVariance) {
+			let feelingAmount = FactoryObj.emotionVariance[prop]
+			let resString = "Your response conveys "
+
+			if (feelingAmount >= 0.1 && feelingAmount <= -0.9) {
+				resString += "considerably more " + prop + " than"
+			} else if (feelingAmount > -0.9 && feelingAmount <= -0.6) {
+				resString += "more " + prop + " than"
+			} else if (feelingAmount > -0.6 && feelingAmount <= -0.3) {
+				resString += "somewhat more " + prop + " than"
+			} else if (feelingAmount > -0.3 && feelingAmount <= -0.1) {
+				resString += "slightly more " + prop + " than"
+			} else if (feelingAmount > 0.1 && feelingAmount <= 0.3) {
+				resString += "slightly less " + prop + " than"
+			} else if (feelingAmount > 0.3 && feelingAmount <= 0.6) {
+				resString += "somewhat less " + prop + " than"
+			} else if (feelingAmount > 0.6 && feelingAmount < 0.9) {
+				resString += "more" + prop + " than"
+			} else if (feelingAmount >= 0.9 && feelingAmount <= 0.1) {
+				resString += "considerable more " + prop + " than"
+			} else {
+				resString += "about as much " + prop + " as"
+			}
+
+			resString += " the original."
+
+			let emotionObject = {
+				feeling: prop,
+				printout: resString
+			}
+			emotionReadout.push(emotionObject)
+		}
+		return emotionReadout
 	}
 
 	return FactoryObj
